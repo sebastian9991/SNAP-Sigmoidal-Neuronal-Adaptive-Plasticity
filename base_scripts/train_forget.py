cd0c37d82c9ca276e4e5012ed6b453a39c7e35f5
Sebastian C. Sabry <sebastiancsabry@system76-pc.localdomain>
2025-05-10 14:30:22 -0400

    added interfaces for better OOP

--- base_scripts/train_forget.py
+++ base_scripts/train_forget.py
@@ -6,10 +6,11 @@ from experiments.forget_softhebb_experiment import ForgetExperiment
 from interfaces.experiment import Experiment
 from interfaces.network import Network
 from models.hebbian_network import HebbianNetwork
-from models.MLP.models import NewMLPBaseline_Model
 from utils.experiment_logger import *
 from utils.experiment_parser import *
 from utils.experiment_stats import *
+
+from models.MLP.models import NewMLPBaseline_Model
 
 # Create log
 results_log = configure_logger("Forget Result Log", "./results/results_forget.log")
@@ -18,29 +19,9 @@ results_log = configure_logger("Forget Result Log", "./results/results_forget.lo
 ARGS = parse_arguments()
 
 
-# Main Code
 def main():
     # Parallel training
     train_acc_lists, test_acc_lists = parallel_training(ARGS, 1)
-
-    # Calculate and log averages and variances for each digit pair
-
-
-#    digit_pairs = [
-#        (0, 1, test_acc_lists[0], train_acc_lists[0]),
-#        (2, 3, test_acc_lists[1], train_acc_lists[1]),
-#        (4, 5, test_acc_lists[2], train_acc_lists[2]),
-#        (6, 7, test_acc_lists[3], train_acc_lists[3]),
-#        (8, 9, test_acc_lists[4], train_acc_lists[4])
-#    ]
-
-#    for digit_pair in digit_pairs:
-#        digit_1, digit_2, test_acc, train_acc = digit_pair
-#        avg_test = average(test_acc)
-#        var_test = variance(test_acc)
-#        avg_train = average(train_acc)
-#        var_train = variance(train_acc)
-#        results_log.info(f"Digits {digit_1} and {digit_2} || Epoch: {ARGS.epochs} || Lambda: {ARGS.heb_lamb} || Dataset: {ARGS.data_name.upper()} || Inhibition: {ARGS.heb_inhib.lower().capitalize()} || Learning Rule: {ARGS.heb_learn.lower().capitalize()} || Function Type: {ARGS.heb_growth.lower().capitalize()} || Experiment Type: {ARGS.experiment_type.lower().capitalize()} || Test Acc: avg = {avg_test}, var = {var_test} || Train Acc: avg = {avg_train}, var = {var_train}")
 
 
 # Model Training
@@ -49,10 +30,6 @@ def train_and_eval(args: Tuple) -> List[List[float]]:
 
     num: int
     params, num = args
-    # print(params)
-    # import sys
-    # sys.stdout.flush()
-    # return
     model: Network = NewMLPBaseline_Model(
         params.K,
         params.focus,
@@ -73,7 +50,6 @@ def train_and_eval(args: Tuple) -> List[List[float]]:
     accuracies = list(experiment.run())
     experiment.cleanup()
 
-    # Return accuracies split by digit pairs
     return [
         accuracies[0:5],  # Test accuracies for digit pairs 0-1, 2-3, 4-5, 6-7, 8-9
         accuracies[5:10],  # Train accuracies for digit pairs 0-1, 2-3, 4-5, 6-7, 8-9
@@ -103,7 +79,6 @@ def parallel_training(
     return train_acc_lists, test_acc_lists
 
 
-# When to run code
 if __name__ == "__main__":
     main()
     print("Process Completed.")
