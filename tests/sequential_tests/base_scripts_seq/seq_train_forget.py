74d589cedd8a7c6c136b1e552f11351d4c454321
Sebastian C. Sabry <sebastiancsabry@system76-pc.localdomain>
2025-05-10 15:45:45 -0400

    updating .gitignore to ignore pycharm added files

---
+++ tests/sequential_tests/base_scripts_seq/seq_train_forget.py
@@ -0,0 +1,52 @@
+import argparse
+from typing import List, Tuple
+
+from experiments.forget_softhebb_experiment import ForgetExperiment
+
+from interfaces.experiment import Experiment
+from interfaces.network import Network
+from models.MLP.models import MLPBaseline
+from utils.experiment_utils.experiment_logger import configure_logger
+from utils.experiment_utils.experiment_parser import parse_arguments_from_list
+from utils.experiment_utils.experiment_stats import *
+
+# Create log
+results_log = configure_logger("Forget Result Log", "./results/results_forget.log")
+
+
+def run_experiment_direct(
+    arg_list: List[str],
+) -> Tuple[List[List[float]], List[List[float]]]:
+    params = parse_arguments_from_list(arg_list)
+
+    model: Network = MLPBaseline(
+        params.K,
+        params.focus,
+        params.hsize,
+        params.lamb,
+        params.w_lr,
+        params.b_lr,
+        params.l_lr,
+        params.nclasses,
+        params.device,
+        params.weight_growth,
+    )
+
+    experiment: Experiment = ForgetExperiment(
+        model,
+        params,
+        f"-{params.experiment_name}-{params.experiment_type.lower()}-{params.lr}--{params.heb_learn.lower()}-{params.heb_growth.lower()}-{params.heb_focus.lower()}-{params.heb_inhib.lower()}-{params.heb_lamb}---{params.class_learn.lower()}-{params.class_growth.lower()}-{params.class_focus.lower()}-0",
+    )
+    accuracies = list(experiment.run())
+    experiment.cleanup()
+
+    test_acc = accuracies[0:5]
+    train_acc = accuracies[5:10]
+    return train_acc, test_acc
+
+
+if __name__ == "__main__":
+    import sys
+
+    run_experiment_direct(sys.argv[1:])
+    print("Process Completed.")
